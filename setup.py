d07ee5e55cadb89fef749af8cb2db3c949cfcdea
Simon Ward <2798086+wardsimon@users.noreply.github.com>
2022-02-15 12:37:10 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,12 +1,19 @@
 from setuptools import setup
+# read the contents of your README file
+from pathlib import Path
+this_directory = Path(__file__).parent
+long_description = (this_directory / "README.md").read_text()
 
 setup(
     name='gfConical',
     version='v0.0.1',
     packages=['gfConical'],
-    url='',
+    url='https://github.com/wardsimon/gfConical/',
     license='BSD3',
     author='simonward',
     author_email='',
     description='Connect and control the Grainfather conical fermenter'
+    long_description=long_description,
+    long_description_content_type='text/markdown'
+    install_requires=[requests]
 )
